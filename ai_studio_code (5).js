72bd843860dc854a0560564ada7c82b793a8bc37
raikwarsahil15-jpg <raikwarsahil15@gmail.com>
2026-09-08 14:56:00 +0530

    Delete ai_studio_code (5).js

--- ai_studio_code (5).js
+++
@@ -1,5 +0,0 @@
-const updateOrderStatus = async (orderId, newStatus) => {
-  // 1. Update order status in Firestore
-  // 2. If 'Delivered', add to User's 'totalSpent' for Weekly Top Customer tracking
-  // 3. Send notification to user
-};
