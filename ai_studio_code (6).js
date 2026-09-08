2e0b019a8e6a5770d9db0816b00bc58c63d51817
raikwarsahil15-jpg <raikwarsahil15@gmail.com>
2026-09-08 14:56:12 +0530

    Delete ai_studio_code (6).js

--- ai_studio_code (6).js
+++
@@ -1,6 +0,0 @@
-// Function in Checkout logic
-const validateVipAccess = async (user, product) => {
-  if (product.isVIP && !user.isVIP) {
-    throw new Error("Access Denied: VIP Membership Required");
-  }
-};
